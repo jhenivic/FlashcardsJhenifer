49be0c2aa28dfff3dc17e58131c74d2b637d2e67
jhenivic <168014351+jhenivic@users.noreply.github.com>
2024-11-01 11:41:03 -0300

    cores

---
+++ flashcardJhenifer/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom Quixote'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou "Guernica"?',
+    'Pablo Picasso'
+)
+
+criaCartao(
+    'Filosofia',
+    'Júpiter e Plutão são os correlatos romanos de quais deuses gregos?',
+    ' Zeus e Hades'
+)
+
+criaCartao(
+    'Geografia',
+    ' Que cidade brasileira é conhecida por chover todos os dias quase à mesma hora?',
+    'Belém'
+)
